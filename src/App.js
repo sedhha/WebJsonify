ad9de7a9ac99ca4fceffc76589ff2131c440b160
Nicolas <nicolas@racchi.com>
2020-09-26 15:28:02 +0200

    Template skeleton and first components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
+import Header from "./components/Header/Header";
+import GlobalSiteConstants from "./components/ConstantFiles/GlobalSiteConstants";
+import HomePage from "./components/HomePage";
+import LoginPage from "./components/LoginPage";
+import { BrowserRouter, Route } from "react-router-dom";
 
-import Header from './components/Header/Header';
-import GlobalSiteConstants from './components/ConstantFiles/GlobalSiteConstants';
-import HomePage from './components/HomePage';
-import LoginPage from './components/LoginPage';
-import {BrowserRouter, Route} from 'react-router-dom';
+// Templates
+import CV1 from "./templates/cv_1/CV1";
 
 function App() {
   return (
     <div>
-    <BrowserRouter>
-    <Header HeaderNames = {GlobalSiteConstants.HeaderNames}
-    HeaderHrefs = {GlobalSiteConstants.HeaderHrefs}/>
-    <Route exact path = "/"><HomePage / ></Route>
-    <Route exact path = "/login"><LoginPage / ></Route>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Header
+          HeaderNames={GlobalSiteConstants.HeaderNames}
+          HeaderHrefs={GlobalSiteConstants.HeaderHrefs}
+        />
+
+        <Route exact path="/">
+          <HomePage />
+        </Route>
+        <Route exact path="/login">
+          <LoginPage />
+        </Route>
+
+        {/* Temporary: needs API protection */}
+        <Route exact path="/cv1" component={CV1} />
+      </BrowserRouter>
     </div>
   );
 }
